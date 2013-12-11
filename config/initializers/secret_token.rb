55b30ab238d2ca6d126b93531c2593be7721867a
Denis Krivostchokov <densomart@gmail.com>
2013-12-11 12:57:08 +0700

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LastSpree::Application.config.secret_key_base = 'b6d28c56f4dd3b32fd14e7a069eb49b17590852448c557b5e1fcd2fa6c7b4325c2cdd487ab4b87e4077dbbedc8a287e5603c858464d56541aec3893e624e7d1f'
