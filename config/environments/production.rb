f428685266a9cc01914182b5e9966b4e91f549e0
Denis Krivostchokov <densomart@gmail.com>
2013-12-16 13:15:48 +0700

    correct awesome font precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,14 +27,10 @@ LastSpree::Application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.assets.compile = true
 
   # Version of your assets, change this if you want to expire all your assets.
   config.assets.version = '1.0'
-
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
